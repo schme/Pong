622dd8a2ca99b6eeb6a1624d4f55b89b99e2300c
Kasper Sauramo <kasper.sauramo@cs.helsinki.fi>
2013-05-17 16:36:29 +0300

    Oops

--- Pong/src/main/java/com/laughingmasq/pong/game/Ball.java
+++ Pong/src/main/java/com/laughingmasq/pong/game/Ball.java
@@ -1,4 +1,3 @@
-
 package com.laughingmasq.pong.game;
 
 /**
@@ -6,6 +5,20 @@ package com.laughingmasq.pong.game;
  */
 public class Ball {
 
-    public Ball() {}
+    private int posX = 0;
+    private int posY = 0;
+
+    public Ball() {
+
+    }
+
+    public int getPosX() {
+        return posX;
+    }
+
+    public int getPosY() {
+        return posY;
+    }
+
 
 }
