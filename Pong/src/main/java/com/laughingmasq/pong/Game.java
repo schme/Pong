06c4f7367937794ef44dcb9d02efeff887fdb69a
Kasper Sauramo <kasper.sauramo@cs.helsinki.fi>
2013-05-20 06:51:32 +0300

    More pom work.

--- Pong/src/main/java/com/laughingmasq/pong/Game.java
+++ Pong/src/main/java/com/laughingmasq/pong/Game.java
@@ -3,18 +3,15 @@ package com.laughingmasq.pong;
 import com.laughingmasq.pong.game.Board;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-import javax.swing.Timer;
 
 /**
  * @author schme
  */
 public class Game implements ActionListener {
 
-    private Timer timer;
-    private Board board;
+    private Board board = null;
 
     public Game() {
-        this.timer = new Timer(16, this); // ~60fps
         this.board = new Board();
     }
 
